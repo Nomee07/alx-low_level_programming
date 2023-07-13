addcdc97a9a7c5e538457dcf2a946f7c8b144e57
Nomee07 <gigabanomfundo@gmail.com>
2023-07-13 14:22:41 -0700

    1-10quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: alaways 1 (Success)
+ * Return: alaways 0 (Success)
  */
 int main(void)
 {
-	fput(2,
+	write(2,
 	"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
         return (1);
 }
