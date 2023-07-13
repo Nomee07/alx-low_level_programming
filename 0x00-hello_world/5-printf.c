d4900a36912b8cca938088450a624c6a27362824
Nomee07 <gigabanomfundo@gmail.com>
2023-07-13 08:35:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return : 0 when successful
+ * description: Displays a string using printf
+ */
+int main(void)
+{i
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
