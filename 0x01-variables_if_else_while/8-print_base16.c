52deecfc749e600c53c496bc62acdd294cbacc55
Nomee07 <gigabanomfundo@gmail.com>
2023-07-14 15:48:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
