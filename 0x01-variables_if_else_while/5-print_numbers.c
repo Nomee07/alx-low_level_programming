45b62b464ee79b93c109c803f1e52524b02cef6a
Nomee07 <gigabanomfundo@gmail.com>
2023-07-14 14:39:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit <= 9; digit++)
+{
+putchar(digit + '0');
+}
+putchar('\n');
+return (0);
+}
