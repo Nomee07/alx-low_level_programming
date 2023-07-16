b01d7fc1ff4bb08267f534f0505fb006f38f96ab
Nomee07 <gigabanomfundo@gmail.com>
2023-07-16 04:33:27 -0700

    100-print_comb.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(',');
+}
+}
+putchar('\b');
+putchar('\n');
+return (0);
+}
