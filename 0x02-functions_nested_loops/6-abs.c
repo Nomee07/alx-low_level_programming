721df923b3ae0ba95ef0db7608e1dc4b0d825f13
Nomee07 <gigabanomfundo@gmail.com>
2023-07-19 07:47:41 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - Computes the absolute value of an integer
+ *@c: The number of the computer
+ *Return: Absolute value of the number or zero
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_value;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
