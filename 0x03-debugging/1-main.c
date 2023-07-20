05f0df5cf8ec0f360650082de8f849613d5e41a1
Nomee07 <gigabanomfundo@gmail.com>
2023-07-20 12:59:14 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,11 +8,11 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-//Commenting out the while loop to avoid the infinite loop
-//while (i < 10)
-//{
-//putchar(i);
-//}
+/* Commenting out the while loop to avoid the infinite loop */
+/* while (i < 10) */
+/* { */
+/* putchar(i); */
+/* } */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
