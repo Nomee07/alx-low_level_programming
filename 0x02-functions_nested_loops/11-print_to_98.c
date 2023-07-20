c5092e5145cd4f82467806e0e601a0db5b366050
Nomee07 <gigabanomfundo@gmail.com>
2023-07-20 01:42:50 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,24 @@
  */
 void print_to_98(int n)
 {
+int i;
 if (n <= 98)
 {
-for (n = n; n <= 97; n++)
-printf("%d, ", n);
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
 }
 else
 {
-for (n = n; n >= 98; n--)
-printf("%d, ", n);
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
 }
-printf("98\n");
+}
+printf("\n");
 }
