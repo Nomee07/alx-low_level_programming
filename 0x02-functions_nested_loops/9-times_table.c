2c003a7750cf75c2250ec144eb0812f7fed24cec
Nomee07 <gigabanomfundo@gmail.com>
2023-07-19 19:08:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,21 +15,21 @@ if (z > 9)
 {
 u = z % 10;
 d = (z - u) / 10;
-putchar(',');
-putchar(' ');
-putchar(d + '0');
-putchar(u + '0');
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
 }
 else
 {
 if (y != 0)
 {
-putchar(',');
-putchar(' ');
-putchar(' ');
+_putchar(44);
+_putchar(32);
+_putchar(32);
 }
-putchar(z + '0');
+_putchar(z + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
