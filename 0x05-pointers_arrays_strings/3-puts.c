b814ab136e65ad7bcc68530eb5e88149f90b5752
Nomee07 <gigabanomfundo@gmail.com>
2023-07-25 23:56:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ *
+ * @str: Pointer to the string to be printed.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	putchar(*str);
+	str++;
+	}
+	putchar('\n');
+}
