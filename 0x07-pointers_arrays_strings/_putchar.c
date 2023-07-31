8a6bab8200cc2eb354bcbe9440e5d53ee9917dd7
Nomee07 <gigabanomfundo@gmail.com>
2023-07-31 06:52:08 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 0n success 1
+ * On error, -1 is returned, and errno is set appropritely.
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
