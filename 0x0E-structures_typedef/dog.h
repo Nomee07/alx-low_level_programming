38562fdbdce07862a501c97e839ee14817217a8a
Nomee07 <gigabanomfundo@gmail.com>
2023-08-14 10:41:34 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** struct_dog - A new type.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @0wner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
