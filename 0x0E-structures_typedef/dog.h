cf97a345a1ce3684ccf95fa46649a3abc16bacf7
Nomee07 <gigabanomfundo@gmail.com>
2023-08-15 04:36:06 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - A structure to represent information about about a dog.
  * @name: The name of the dog.
