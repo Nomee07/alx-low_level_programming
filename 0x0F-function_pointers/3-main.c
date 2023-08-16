7da814cd629dca4c618d1b670c1f2895440b6e7e
Nomee07 <gigabanomfundo@gmail.com>
2023-08-16 16:54:16 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,49 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the calculator program.
+ * @argc: The number of command line arguments.
+ * @argv: An array of command line argument strings.
+ *
+ * Return: 0 if successful or other values on error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
