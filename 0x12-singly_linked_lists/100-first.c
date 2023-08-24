552f3f9725287fcce480bda1a0e27c2bf5b04845
Nomee07 <gigabanomfundo@gmail.com>
2023-08-24 09:28:01 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * startup_function - Prints start up message before main is executed.
+ */
+
+void startup_function(void) __attribute__((constructor));
+
+void startup_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
