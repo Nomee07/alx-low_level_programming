d5ad6ca68468a188bd684401bdd62b77506a7b6d
Nomee07 <gigabanomfundo@gmail.com>
2023-08-24 09:54:15 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,21 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("[0](nil)\n");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 
-		count++;
+		nodes++;
 		h = h->next;
 	}
-	return (count);
+	return (nodes);
 }
