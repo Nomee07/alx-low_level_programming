7ea7a61e911c81d32e9f907137ecbb5a99f89f51
Nomee07 <gigabanomfundo@gmail.com>
2023-08-24 16:22:22 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
+void startup_function(void) __attribute__((constructor));
+
 /**
  * startup_function - Prints start up message before main is executed.
  */
-
-void startup_function(void) __attribute__((constructor));
 
 void startup_function(void)
 {
