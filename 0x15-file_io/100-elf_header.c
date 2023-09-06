80b749257d533dc613b806560c275cd28606b061
Nomee07 <gigabanomfundo@gmail.com>
2023-09-06 06:39:56 -0700

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -24,14 +24,41 @@ void print_elf_info(const Elf64_Ehdr *header)
 	printf("\n");
 
 	printf("Class:                             %s\n", header->e_ident[EI_CLASS]
-			== ELFCLASS32 ? "ELF32" : "ELF64");
+			== ELFCLASS32 ? "ELF32" : (header->e_ident[EI_CLASS] == ELFCLASS64 ?
+				"ELF64" : "Unknown"));
 	printf("Data:                              %s\n", header->e_ident[EI_DATA] ==
 			ELFDATA2LSB ? "2's complement, little-endian" :
-			"2's complement, big-endian");
+			(header->e_ident[EI_DATA] == ELFDATA2MSB ?
+			 "2's complement, big-endian" : "Unknown"));
 	printf("Version:                           %d (current)\n",
 			header->e_ident[EI_VERSION]);
 	printf("OS/ABI:                            %d\n",
 			header->e_ident[EI_OSABI]);
+	switch (header->e_ident[EI_OSABI])
+	{
+		case ELFOSABI_SYSV:
+			printf(" (UNIX System V)");
+			break;
+		case ELFOSABI_NETBSD:
+			printf(" (NetBSD)");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf(" (Solaris)");
+			break;
+		case ELFOSABI_SORTIX:
+			printf(" (Sortix)");
+			break;
+		case ELFOSABI_SPARC:
+			printf(" (SPARC big-endian)");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf(" (Standalone)");
+			break;
+		default:
+			printf(" (Unknown)");
+			break;
+	}
+	printf("\n");
 	printf("ABI Version:                       %d\n",
 			header->e_ident[EI_ABIVERSION]);
 	printf("Type:                              %s\n",
